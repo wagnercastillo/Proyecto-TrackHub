91c86d0cdfb1ecd9e78491c1239c56b707655d17
JorgePoma <jorge.poma@unl.edu.ec>
2021-02-06 14:33:31 -0500

    vistas inicio-registro

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,10 @@ const router = Router()
 router.get('/users/signin',(req, res) => {
     res.render('users/signin');
 });
-router.get('/users/signout',(req, res) => {
-    res.render('users/signout');
+router.get('/users/signup',(req, res) => {
+    res.render('users/signup');
 });
+
+
 
 module.exports = router;
