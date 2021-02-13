d907595001c1f71d783e5749910a86f3a64aa2b1
wagner.castillo@unl.edu.ec <66141549+wagnercastillo@users.noreply.github.com>
2021-02-13 01:54:12 -0500

    Integracion de Unidades

--- src/routes/cooperativa.routes.js
+++ src/routes/cooperativa.routes.js
@@ -1,18 +1,22 @@
-const {Router} =require('express');
+const {
+    Router
+} = require('express');
 const router = Router();
 
 import * as cooperativaControllers from '../controllers/cooperativa.controllers'
-import {verifyToken} from '../middlewars'
+import {
+    verifyToken
+} from '../middlewars'
 router.get('/', cooperativaControllers.Principal)
 
-router.get('/guardarCooperativa/add',cooperativaControllers.getCooperativaPrincipal)
+router.get('/guardarCooperativa/add', cooperativaControllers.getCooperativaPrincipal)
 
 router.post('/guardarCooperativa/add', cooperativaControllers.createCooperativa)
- 
+
 router.get('/enabledCooperativa/:id', cooperativaControllers.enabledCooperativa)
 
 router.get('/editar/:id', cooperativaControllers.updateCooperativaById)
 
 router.delete('/:cooperativaId', cooperativaControllers.deleteCooperativaById)
 
-module.exports= router;
+module.exports = router;
